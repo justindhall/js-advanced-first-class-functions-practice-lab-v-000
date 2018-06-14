031715e587b8f84749b91af29a650f579d64187b
Justin Hall <hall.justin.d@gmail.com>
2018-06-14 17:55:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -20,9 +20,7 @@ function logDriversByHometown(drivers, location){
 
 function driversByRevenue(drivers){
   
-  let sortedDrivers = drivers;
-  
-  return sortedDrivers.sort(function (driver1, driver2) {
+  return drivers.slice().sort(function (driver1, driver2) {
     return driver1.revenue - driver2.revenue;
   })
   
