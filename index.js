1357addf86272eaafee68cca640846fcecaa1a8b
Justin Hall <hall.justin.d@gmail.com>
2018-06-14 17:46:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solution in this file!
+
+function logDriverNames(drivers){
+  
+  return drivers.forEach(function (driver) {console.log(driver.name)});
+  
+}
+
+function logDriversByHometown(drivers, location){
+  
+  return drivers.forEach( function (driver) {
+    
+    if (driver.hometown === location){
+      console.log(driver.name)
+    }
+    
+  })
+  
+}
