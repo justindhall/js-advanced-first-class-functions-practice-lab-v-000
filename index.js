5eb56d536182e1b9e0140e87953f012b23058802
Justin Hall <hall.justin.d@gmail.com>
2018-06-14 18:00:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -28,6 +28,8 @@ function driversByRevenue(drivers){
 
 function driversByName(drivers){
   
-  
+  return drivers.slice().sort(function (driver1, driver2) {
+    return driver1.name.localeCompare(driver2.name);
+  })
   
 }
